96b9b2e232c1d9b2566c2f4296528ffa23246bf8
Sainath Prasanna <sainathprasanna@gmail.com>
2016-10-05 16:28:46 +0530

    Aospa setup

--- AndroidProducts.mk
+++ AndroidProducts.mk
@@ -1,4 +1,5 @@
-# Copyright (C) 2015 The CyanogenMod Project
+#
+# Copyright 2013 The Android Open Source Project
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,16 +12,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+#
 
-$(call inherit-product, device/motorola/osprey/full_osprey.mk)
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/full_osprey.mk
 
-# Boot animation
-TARGET_SCREEN_WIDTH := 720
-TARGET_SCREEN_HEIGHT := 1280
 
-## Device identifier. This must come after all inclusions
-PRODUCT_DEVICE := osprey
-PRODUCT_NAME := cm_osprey
-PRODUCT_BRAND := Motorola
-PRODUCT_MANUFACTURER := Motorola
-PRODUCT_RELEASE_NAME := osprey
